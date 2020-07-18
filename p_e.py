83dbf1cbb4b38a464f633a738f5df382ce01fc25
nupur <nupurrathi2218@gmail.com>
2020-07-18 23:51:14 +0530

    added brightness option successfully

--- p_e.py
+++ p_e.py
@@ -3,6 +3,7 @@ from tkinter import filedialog, Canvas
 from tkinter import messagebox
 import tkinter.font as font
 import cv2
+import numpy as np
 
 root = Tk()
 canvas = Canvas(root, width=500, height=600)
@@ -16,11 +17,15 @@ bfont2 = font.Font(family='Bradley Hand ITC', size=20, weight='bold')
 image = cv2.imread('bg2.png')
 i = cv2.imread('bg2.png')
 blurred = cv2.imread('bg2.png')
+bright= cv2.imread('bg2.png')
 x=2
+x1=0
+x2=0
 canvas3 = Canvas(root, width=500, height=600)
 int_rot=0
 int_blur=0
 int_ok=0
+int_bright=0
 
 def back(canvas3,canvas2):
       canvas3.pack_forget()
@@ -40,24 +45,32 @@ def writename():
     root2.mainloop()
 def done():
     global int_ok
-    global image, blurred,i
+    global image, blurred,i,bright
     if(int_ok==1):
         image=blurred
         i=blurred
-
+        bright=blurred
     if(int_ok==2):
         image=i
         blurred=i
+        bright=i
+    if(int_ok==3):
+        image= bright
+        i=bright
+        blurred=bright
+        
+
 
 
 def dialogbox():
        filename = filedialog.askopenfilename(initialdir="E:\"", title='Select a image file', filetypes=(("jpg files", "*.jpg"), ("png files", "*.png")))
        if(filename!=''):
          img = cv2.imread(filename)
-         global image,i,blurred
+         global image,i,blurred,bright
          image=img
          i=img
          blurred=img
+         bright=img
          cv2.imshow('image', image)
          global int_chosefile
          int_chosefile=1
@@ -86,15 +99,27 @@ def rot():
     global image
     global bluradd
     global blurneg
+    global briadd,brineg,ok
     global rot_n_c,rot_n_cc,rot_e,flip_y,flip_x,flip_xy
-    global l
-    global int_blur,int_rot
+    global l,l1
+    global int_blur,int_rot,int_bright,x,x1,x2
+    x=0
+    x1=0
+    x2=0
     int_rot=1
     cv2.imshow('image', image)
     if(int_blur==1):
       bluradd.place_forget()
       blurneg.place_forget()
       l.place_forget()
+      int_blur=0
+      ok.place_forget()
+    if(int_bright==1):
+        briadd.place_forget()
+        brineg.place_forget()
+        l1.place_forget()
+        int_bright=0
+        ok.place_forget()
     rot_n_c.place(x=280,y=45)
     rot_n_cc.place(x=280, y=115)
     rot_e.place(x=280, y=185)
@@ -134,26 +159,111 @@ def neg():
   blurred = cv2.blur(image, ksize=(x, x))
   cv2.imshow('image', blurred)
   int_ok=1
+
+def addb():
+  global int_ok
+  global x1,x2
+  global image
+  global l1
+  global bright
+  if (x1 <= 100):
+    x1=x1+1
+    l1.place_forget()
+    l1= Label(canvas3, text=x1, font=bfont)
+    l1.place(x=400, y=200)
+  else:
+      messagebox.showwarning("WARNING","Cannot increse brightness value above 100")
+
+  bright = np.asarray(image)
+  bright = np.where((bright + x1) < bright, 255, (bright + x1))
+  cv2.imshow('image', bright)
+  int_ok=3
+  x2=0
+
+def negb():
+  global int_ok
+  global x2,x1
+  global image
+  global l1
+  global bright
+  if (x2 <= 100):
+    x2=x2+1
+    l1.place_forget()
+    l1= Label(canvas3, text=x2, font=bfont)
+    l1.place(x=400, y=200)
+  else:
+      messagebox.showwarning("WARNING","Cannot decrease brightness value below 100")
+
+  bright = np.asarray(image)
+  bright = np.where((bright - x2) > bright, 0 , (bright - x2))
+
+  cv2.imshow('image', bright)
+  int_ok=3
+  x1=0
+
+
 def blur():
     global image
-    global int_blur,int_rot
+    global int_blur,int_rot,int_bright,x1,x2
     int_blur=1
-    global bluradd
+    global bluradd,blurneg
     global rot_n_c,rot_n_cc,rot_e,flip_y,flip_x,flip_xy
-    cv2.imshow('image',image)
-    bluradd.place(x=350,y=45)
-    blurneg.place(x=350, y=115)
-    l.place(x=700,y=0)
-    ok.place(x=350,y=500)
-    if(int_rot==1):
+    global briadd,brineg
+    global l1
+    cv2.imshow('image', image)
+    x1=0
+    x2=0
+    if (int_rot == 1):
         rot_n_c.place_forget()
         rot_n_cc.place_forget()
         rot_e.place_forget()
         flip_y.place_forget()
         flip_x.place_forget()
         flip_xy.place_forget()
+        ok.place_forget()
+        int_rot=0
+    if(int_bright==1):
+        briadd.place_forget()
+        brineg.place_forget()
+        ok.place_forget()
+        int_bright=0
+        l1.place_forget()
 
+    bluradd.place(x=350,y=45)
+    blurneg.place(x=350, y=115)
+    l.place(x=700,y=0)
+    ok.place(x=350,y=500)
 
+def brit():
+    global image
+    global int_blur, int_rot, int_bright
+    int_bright = 1
+    global bluradd, blurneg
+    global rot_n_c, rot_n_cc, rot_e, flip_y, flip_x, flip_xy
+    global briadd, brineg
+    global l,l1,x
+    x=0
+    cv2.imshow('image', image)
+    if (int_rot == 1):
+        rot_n_c.place_forget()
+        rot_n_cc.place_forget()
+        rot_e.place_forget()
+        flip_y.place_forget()
+        flip_x.place_forget()
+        flip_xy.place_forget()
+        ok.place_forget()
+        int_rot = 0
+    if (int_blur == 1):
+        bluradd.place_forget()
+        blurneg.place_forget()
+        ok.place_forget()
+        int_blur = 0
+        l.place_forget()
+
+    briadd.place(x=270, y=45)
+    brineg.place(x=270, y=115)
+    l1.place(x=700, y=0)
+    ok.place(x=350, y=500)
 
 
 
@@ -172,7 +282,7 @@ def editoptions(canvas2):
        button_filter= Button(canvas3, text="FILTERS", width=12, height=0, bd=2, font=bfont2, bg='teal').place(x=15,y=115)
        button_draw = Button(canvas3, text="DRAW", width=12, height=0, bd=2, font=bfont2, bg='teal').place(x=15,y=185)
        button_rotate = Button(canvas3, text="ROTATE", width=12, height=0, bd=2, font=bfont2, bg='teal', command=lambda:rot()).place(x=15,y=255)
-       button_brightness = Button(canvas3, text="BRIGHTNESS", width=12, height=0, bd=2, font=bfont2, bg='teal').place(x=15,y=325)
+       button_brightness = Button(canvas3, text="BRIGHTNESS", width=12, height=0, bd=2, font=bfont2, bg='teal', command=lambda:brit()).place(x=15,y=325)
        button_save = Button(canvas3, text="SAVE", width=12, height=0, bd=2, font=bfont2, bg='teal',command=lambda:writename()).place(x=15,y=395)
        button_back = Button(canvas3, text="BACK", width=12, height=0, bd=2, font=bfont2, bg='teal',command=lambda:back(canvas3,canvas2)).place(x=15,y=465)
 def edit():
@@ -186,8 +296,13 @@ def edit():
     open_imgb= Button(canvas2, text="Open Images",width=12, height=0 ,bd=2, font=bfont, bg='teal',command=lambda:dialogbox()).place(x=100,y=150)
     open_vidb = Button(canvas2, text="Open Videos", width=12, height=0, bd=2, font=bfont, bg='teal',command=lambda:dialogbox2()).place(x=100,y=270)
     editfile = Button(canvas2, text="EDIT", width=6, height=0, bd=2, font=bfont2, bg='teal',command=lambda:editoptions(canvas2)).place(x=190, y=390)
+
 bluradd = Button(canvas3, text="BLUR +",width=8, height=0 ,bd=2, font=bfont2, bg='teal', command=lambda:add())
 blurneg = Button(canvas3, text="BLUR -", width=8, height=0, bd=2, font=bfont2, bg='teal', command=lambda:neg())
+
+briadd= Button(canvas3, text="BRIGHTNESS +",width=12, height=0 ,bd=2, font=bfont2, bg='teal', command=lambda:addb())
+brineg= Button(canvas3, text="BRIGHTNESS -",width=12, height=0 ,bd=2, font=bfont2, bg='teal', command=lambda:negb())
+
 ok = Button(canvas3, text="OK", width=8, height=0, bd=2, font=bfont2, bg='teal', command=lambda:done())
 
 rot_n_c = Button(canvas3, text="90 CLockwise",width=12, height=0 ,bd=2, font=bfont2, bg='teal', command=lambda:rotate(1))
@@ -198,7 +313,7 @@ flip_x = Button(canvas3, text="flip horizontally",width=12, height=0 ,bd=2, font
 flip_xy = Button(canvas3, text="flip both",width=12, height=0 ,bd=2, font=bfont2, bg='teal', command=lambda:rotate(6))
 
 l=Label()
-
+l1=Label()
 
 button_edit= Button(canvas, text='EDIT', width=185, height=50, image=button_img, bg='white', bd=8, command=edit).place(x=108, y=237)
 root.mainloop()
